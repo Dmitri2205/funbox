349784176bae80c82cde8929539d162b861a243a
Dmitri2205 <61139405+Dmitri2205@users.noreply.github.com>
2020-10-12 19:49:12 +0300

    Add files via upload

--- static_src/App.jsx
+++ static_src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Element from './components/Element';
 import style from "./style/style.css";
 
 export default class App extends React.Component {
@@ -26,7 +27,6 @@ export default class App extends React.Component {
 	    	}
         ]   //Массив информации о товаре
     };
-
     componentDidMount() {
         setTimeout(() => { this.promptAnswer() }, 800); //Выбор недоступной упаковки
     };
@@ -57,28 +57,27 @@ export default class App extends React.Component {
 	    	this.setState({info:arr});
 	    };
     };
-
     handleClick = (value) => { //Обработчик клика
         let selected = this.state;
         console.log(value);
         var sorted = [...this.state.selected, value].sort((a, b) => { return a - b });
         switch (value) {
-            case '1':                    //Кейсы добавления
+            case 1:                    //Кейсы добавления
                 this.setState({ selected: sorted });
                 break;
-            case '2':
+            case 2:
                 this.setState({ selected: sorted });
                 break;
-            case '3':
+            case 3:
                 this.setState({ selected: sorted });
                 break;
-            case "-1":                   //Кейсы удаления
+            case -1:                   //Кейсы удаления
                 this.deleteItem(value);
                 break;
-            case "-2":
+            case -2:
                 this.deleteItem(value);
                 break;
-            case "-3":
+            case -3:
                 this.deleteItem(value);
                 break;
             case 'info':
@@ -89,18 +88,17 @@ export default class App extends React.Component {
             this.userSelect();
         }, 300);
     };
-
     deleteItem = (value) => {    //Инициация удаления из массива корзины
         let index = null;  //Индекс удаляемого элемента
         const { selected } = this.state;
         const searchAndSlice = selected.map((item, i) => {
-            if (value === '-1' && item === '1' && index === null) {
+            if (value === -1 && item === 1 && index === null) {
                 console.log(item, i);
                 index = i;
-            } else if (value === '-2' && item === '2' && index === null) {
+            } else if (value === -2 && item === 2 && index === null) {
                 console.log(item, i);
                 index = i;
-            } else if (value === '-3' && item === '3' && index === null) {
+            } else if (value === -3 && item === 3 && index === null) {
                 console.log(item, i);
                 index = i;
             } else {
@@ -114,14 +112,13 @@ export default class App extends React.Component {
         };
     this.setState({ selected: searchAndSlice.sort((a, b) => { return a - b }) }); //Обновление и сортировка массива корзины
     }
-
     userSelect = () => {    //Инициация создания элементов корзины
         let { selected, cart, info } = this.state;
         const userCart = selected.map((item, i) => { //Добавление в "корзину"
-            if (item === '1') {
+            if (item === 1) {
                 let element = <li key={i}>c {info[0].name}</li>;
                 return element;
-            } else if (item === '2') {
+            } else if (item === 2) {
                 let element = <li key={i}>c {info[1].name}</li>;
                 return element;
             } else {
@@ -138,10 +135,29 @@ export default class App extends React.Component {
 
     leaveEffect=(e,value) => {     //Присвоение CSS анимации после ухода курсора
     	e.target.style.animation = "selectEffect 1s ease-in-out";
-    }
-
+	};
+	elementsCreate = () => {
+		const elements = this.state.info.map((item,i)=>{
+			console.log(elements);
+			return (
+			<Element name={item.name}
+							count={item.count}
+							weight={item.weight}
+							missed={this.state.missed}
+							infoChange={this.infoChange}
+							index={i+1}
+							handleClick={this.handleClick}
+							selectEffect={this.selectEffect}
+							leaveEffect={this.leaveEffect}
+							key={i}
+			/>
+			);
+		});
+		return elements;
+	};
 render() {
-        let { missed, selected, cart,info } = this.state;
+	const { missed, selected, cart,info } = this.state;
+	const elements = this.elementsCreate();
         return (
 	<div className="app">
 			<div className="selected" 
@@ -151,139 +167,9 @@ render() {
 			</div>      
 		    <h4>Ты сегодня покормил кота?</h4>
 		<div className="app__wraper">
-		        <div className="app__wraper_helper">
-		    {/* Вспомогательная обёртка */}
-		        <div className="app__wraper_helper-item" 
-		        style={missed === 1 ? {border:'3px solid gray',animation:"none"}:{border:'3px solid #1698d9'} }
-		        >
-		        <span className='borderPiece' style={missed === 1 ? {backgroundColor:'gray'}:{backgroundColor:'#1698d9'}}></span>
-					<div className="app__wraper_helper-itemText"
-			    		style={missed === 1 ? {filter:'grayscale(100%)'} : {filter:'unset'}}
-				 		onMouseOver={(event)=>{this.selectEffect(event)}}
-				        onMouseLeave={(event)=>{this.leaveEffect(event,'1')}}
-						onClick={(event)=>{this.handleClick('-1')}}>
-							<p style={{paddingTop:'27px'}}>Сказочное заморское яство</p>
-							<h1>Нямушка</h1>
-							<p onClick={(event)=>{this.infoChange(event,"name",0)}}
-							   style={{ position:'relative',zIndex:'9999'}}
-							>
-							с {info[0].name}</p>
-							<p>
-							<b onClick={(event)=>{this.infoChange(event,"count",0)}}
-							   style={{ position:'relative',zIndex:'9999',marginRight:"2px"}}
-							>{info[0].count}</b>порций <br/>	
-							 мышь в подарок
-							</p>	
-		        			<span className="weight" 
-		        				  onClick={(event)=>{this.infoChange(event,"weight",0)}}
-							   	  style={{zIndex:'9999'}}
-							 >
-							 <p>{info[0].weight}</p>
-							 	кг
-							 </span>
-					</div>
-		        </div>
-
-		        <p style={missed === 1 ? {display:"none"} : {display:' '}}
-		       	className="buy">Чего сидишь?Порадуй котэ,<span onClick={(event)=>this.handleClick("1")}>купи</span></p>
-
-		        <p style={missed === 1 ? {display:"none"} : {display:' '}} 
-		        className='description__text'>Печень утки разварная с артишоками.</p>
-		        
-		         <p style={missed === 1 ? {display:' ' ,color:"#1698d9"} : {display:"none"}} 
-		        className='description__text_missed'>Печалька.С {info[0].name} закончился.</p>
-
-		        </div>
-				
-				<div className="app__wraper_helper" >
-			{/* Вспомогательная обёртка */}
-		        <div className="app__wraper_helper-item"
-		         style={missed === 2 ? {border:'3px solid gray',animation:'unset'}:{border:'3px solid #d91667'} }>
-		        <span className='borderPiece' style={missed === 2 ? {backgroundColor:'gray'}:{backgroundColor:'#d91667'}}></span>
-					<div className="app__wraper_helper-itemText"
-					 	 style={missed === 2 ? {filter:'grayscale(100%)'} : {filter:'unset'}}
-						 onMouseOver={(event)=>{this.selectEffect(event)}}
-					     onMouseLeave={(event)=>{this.leaveEffect(event)}}
-						 onClick={(event)=>{this.handleClick('-2')}}
-						 >
-							<p style={{paddingTop:'27px'}}>Сказочное заморское яство</p>
-							<h1>Нямушка</h1>
-							<p onClick={(event)=>{this.infoChange(event,"name",1)}}
-							   style={{ position:'relative',zIndex:'9999'}}>
-							 с {info[1].name}
-							 </p>
-							<p>
-							<b onClick={(event)=>{this.infoChange(event,"count",1)}}
-							   style={{ position:'relative',zIndex:'9999',marginRight:"2px"}}
-							 >{info[1].count}</b> порций <br/>	
-						 	<b>2</b> мыши в подарок
-							</p>	
-		        			<span className="weight" 
-		        				  onClick={(event)=>{this.infoChange(event,"weight",1)}}
-							   	  style={{zIndex:'9999'}}
-		        			>
-		        			<p>{info[1].weight}</p>
-		        				кг
-		        			</span> 
-						</div>	
-		        	</div>
-		      
-		        <p style={missed === 2 ? {display:"none"} : {display:''}}
-		       	className="buy">Чего сидишь?Порадуй котэ,<span onClick={(event)=>this.handleClick("2")}>купи</span></p>
-
-		        <p style={missed === 2 ? {display:"none"} : {display:' '}} 
-		        className='description__text'>Головы щучьи с чесноком да свежайшая сёмгушка.</p>
-		        
-		        <p style={missed === 2 ? {display:' ',color:"#d91667"} : {display:"none"}} 
-		        className='description__text_missed'>Печалька.С {info[1].name} закончился.</p>
-		       
-		        </div>  
-		   
-		        <div className="app__wraper_helper">
-					{/* Вспомогательная обёртка */}
-		        <div className="app__wraper_helper-item"
-		         style={missed === 3 ? {border:'3px solid gray'}:{border:'3px solid #FFF44B'} }
-		        >
-		        <span className='borderPiece' style={missed === 3 ? {backgroundColor:'gray'}:{backgroundColor:'#FFF44B'}}></span>
-					<div className="app__wraper_helper-itemText"
-					 	style={missed === 3 ? {filter:'grayscale(100%)'} : {filter:'unset'}}
-						onMouseOver={(event)=>{this.selectEffect(event)}}
-				        onMouseLeave={(event)=>{this.leaveEffect(event)}}
-						onClick={(event)=>{this.handleClick('-3')}}>
-							<p style={{paddingTop:'27px'}}>Сказочное заморское яство</p>
-							<h1>Нямушка</h1>
-							<p onClick={(event)=>{this.infoChange(event,"name",2)}}
-							   style={{ position:'relative',zIndex:'9999'}}>
-							с {info[2].name}
-							</p>
-							<p>
-							<b onClick={(event)=>{this.infoChange(event,"count",2)}}
-							   style={{ position:'relative',zIndex:'9999',marginRight:"2px"}}
-							>{info[2].count}</b> порций <br/>	
-						 	<b>5</b> мышей в подарок
-						 	<br/>
-						 	заказчик доволен
-							</p>	
-		        			<span className="weight" 
-		        			 onClick={(event)=>{this.infoChange(event,"weight",2)}}
-							 style={{zIndex:'9999'}}
-		        			>
-		        			<p>{info[2].weight}</p>
-		        				кг
-		        			</span> 
-						</div>	
-		        	</div>
-		      
-		        <p style={missed === 3 ? {display:"none"} : {display:''}}
-		       	className="buy">Чего сидишь?Порадуй котэ,<span onClick={(event)=>this.handleClick("3")}>купи</span></p>
-
-		        <p style={missed === 3 ? {display:"none"} : {display:''}} 
-		        className='description__text'>Филе циплят с трюфелями в бульоне.</p>
-		        
-		        <p style={missed === 3 ? {display:' ',color:'#FFF44B'} : {display:"none"}} 
-		        className='description__text_missed'>Печалька.С {info[2].name} закончился.</p>  	</div>
+		       {elements}
  		</div>
-  </div>
+  	</div>
         )
     }
 }
---
+++ static_src/components/Element.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import RactDOM from 'react-dom';
+
+export default function Element(props){
+return(
+<div className="app__wraper_helper">
+<div className="app__wraper_helper-item"
+     style={props.missed === props.index ? {border:'3px solid gray',animation:'unset'}:{border:'3px solid #d91667'} }
+>
+<span className='borderPiece' style={props.missed === props.index ? {backgroundColor:'gray'}:{backgroundColor:'#d91667'}}></span>
+   <div className="app__wraper_helper-itemText"
+        style={props.missed === props.index ? {filter:'grayscale(100%)'} : {filter:'unset'}}
+        onMouseOver={(event)=>{props.selectEffect(event)}}
+        onMouseLeave={(event)=>{props.leaveEffect(event)}}
+        onClick={(event)=>{props.handleClick( 
+            ( props.index * (-1) )
+            )}}
+    >
+           <p style={{paddingTop:'27px'}}>Сказочное заморское яство</p>
+           <h1>Нямушка</h1>
+           <p onClick={(event)=>{props.infoChange(event,"name",props.index)}}
+              style={{ position:'relative',zIndex:'9999'}}>
+            с {props.name}
+            </p>
+           <p>
+           <b onClick={(event)=>{props.infoChange(event,"count",props.index)}}
+              style={{ position:'relative',zIndex:'9999',marginRight:"2px"}}
+            >{props.count}</b> порций <br/>	
+            <b>2</b> мыши в подарок
+           </p>	
+           <span className="weight" 
+                 onClick={(event)=>{props.infoChange(event,"weight",props.index)}}
+                    style={{zIndex:'9999'}}
+           >
+           <p>{props.weight}</p>
+               кг
+           </span> 
+       </div>	
+   </div>
+
+<p style={props.missed === props.index ? {display:"none"} : {display:''}}
+   className="buy">
+      Чего сидишь?Порадуй котэ,
+      <span onClick={(event)=>props.handleClick(props.index)}>купи</span>
+</p>
+ 
+ <p style={props.missed === props.index ? {display:"none"} : {display:' '}} 
+ className='description__text'>Головы щучьи с чесноком да свежайшая сёмгушка.</p>
+
+  <p style={props.missed === props.index ? {display:' ',color:"#d91667"} : {display:"none"}} 
+  className='description__text_missed'>Печалька.С {props.name} закончился.</p>
+    </div>
+ )
+}
